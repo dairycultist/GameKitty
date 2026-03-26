cc8d4f8e6b0c7bb14d06a86e11152f2d171bef72
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-03-26 19:05:51 -0400

    Clean up

--- asm_to_bin.c
+++ asm_to_bin.c
@@ -3,11 +3,9 @@
 
 #include "asm_to_bin.h"
 
-// KEYDOWN down
-// JMPFALSE no_down
-// ADDC $0 1 /* adds to register $0, carrying into $1 (little-endian) */
-// :no_down
-// SPR 0 0 $0 /* inputs an unsigned byte for sprite index and two signed shorts (as words) for position; automatically culls if outside of screen */
+#define FINISH 0x00
+#define SPR_III 0x01
+#define SPR IAA 0x02
 
 int asm_to_bin(const char *asm_path, unsigned char start_instr[65536], unsigned char frame_instr[65536]) {
 
@@ -20,39 +18,39 @@ int asm_to_bin(const char *asm_path, unsigned char start_instr[65536], unsigned 
 	}
 
 	char token[64];
-	int ignore = 0;
+	int comment_layer = 0;
 	int instr_i = 0;
   
     while (fscanf(f, "%63s", token) != EOF) {
 
 		if (!strcmp(token, "/*")) {
-			ignore = 1;
+			comment_layer++;
 			continue;
 		}
 		
 		if (!strcmp(token, "*/")) {
-			ignore = 0;
+			comment_layer--;
 			continue;
 		}
 
-		if (ignore)
+		if (comment_layer)
 			continue;
 
 		// map this instruction (and its arguments, if any) to a binary
 
 		// note:
 		//   (u)i = (unsigned) immediate value
-		//   (u)a = (unsigned) value-at-address (no registers for simplicity)
-		//   0x00 = finish
+		//   (u)a = (unsigned) value-at-address (addresses are always 16 bits, but the data fetched at that address may vary)
+		//   no registers for simplicity
 		
 		if (!strcmp(token, "SPR")) {
 
-			// 0x01 = draw sprite with parameters index(ui8) x(i16) y(i16)
+			// draws a sprite with given parameters
 
-			// SPR 0 20 20
-			// 0x01 0x00 0x0B 0x00 0x0B 0x00
+			// SPR 0  0  0 => SPR_III index(ui8) x(i16) y(i16)
+			// SPR 0 $0 $0 => SPR_IAA index(ui8) x(a16) y(a16) // TODO
 
-			frame_instr[instr_i++] = 0x01;
+			frame_instr[instr_i++] = SPR_III;
 
 			unsigned char index;
 			short x, y;
